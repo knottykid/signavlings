a32c020bec3464b73cdb4b82bc36b26f7c816716
Zuzanna Tarka <zuzanna.tarka@signavio.com>
2021-12-03 15:41:13 +0100

    chackra setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import "./App.css";
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
+    <ChakraProvider>
     <div className="App">
       <h1>Koalas</h1>
     </div>
+    </ChakraProvider>
   );
 }
 
 export default App;
+
