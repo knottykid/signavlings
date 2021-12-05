86bfc736519ec4ae0bcf8e1c7abb132897691604
John Rosario <73444785+knottykid@users.noreply.github.com>
2021-12-05 22:14:05 +0100

    navbar fully responsive with darkmode

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,14 +4,21 @@ import {
   Stack,
   Heading,
   Flex,
-  Text,
   Button,
   useDisclosure,
+  useColorMode,
+  useColorModeValue,
+  IconButton,
 } from "@chakra-ui/react";
+import { FaMoon, FaSun } from "react-icons/fa";
 
 const Navbar = (props) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+
   const handleToggle = () => (isOpen ? onClose() : onOpen());
+  const { toggleColorMode } = useColorMode();
+  const text = useColorModeValue("dark", "light");
+  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
 
   return (
     <Flex
@@ -24,6 +31,17 @@ const Navbar = (props) => {
       color="white"
       {...props}
     >
+      <IconButton
+        size="md"
+        fontSize="lg"
+        aria-label={`Switch to ${text} mode`}
+        variant="ghost"
+        color="current"
+        marginLeft="2"
+        onClick={toggleColorMode}
+        icon={<SwitchIcon />}
+        {...props}
+      />
       <Flex align="center" mr={5}>
         <Heading as="h1" size="lg" letterSpacing={"tighter"}>
           Signavio by SAP
@@ -37,9 +55,7 @@ const Navbar = (props) => {
         alignItems="center"
         flexGrow={1}
         mt={{ base: 4, md: 0 }}
-      >
-        {/* <Text>Seedlings</Text> */}
-      </Stack>
+      ></Stack>
 
       <Box
         display={{ base: isOpen ? "block" : "none", md: "block" }}
@@ -49,10 +65,11 @@ const Navbar = (props) => {
           variant="outline"
           _hover={{ bg: "teal.700", borderColor: "#AD0F5B" }}
         >
-          Create account
+          Seednavio
         </Button>
       </Box>
     </Flex>
   );
 };
+
 export default Navbar;
