4660ca99e76d52e830009b6ad9940faa5eee9a5a
John Rosario <73444785+knottykid@users.noreply.github.com>
2021-12-05 20:17:48 +0100

    Navbar (#6)
    * mui
    
    * Navbar component
    
    Co-authored-by: John Rosario <john.rosario.francisco@sap.com>

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  Box,
+  Stack,
+  Heading,
+  Flex,
+  Text,
+  Button,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+const Navbar = (props) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const handleToggle = () => (isOpen ? onClose() : onOpen());
+
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding={6}
+      bg="#AD0F5B"
+      color="white"
+      {...props}
+    >
+      <Flex align="center" mr={5}>
+        <Heading as="h1" size="lg" letterSpacing={"tighter"}>
+          Signavio by SAP
+        </Heading>
+      </Flex>
+
+      <Stack
+        direction={{ base: "column", md: "row" }}
+        display={{ base: isOpen ? "block" : "none", md: "flex" }}
+        width={{ base: "full", md: "auto" }}
+        alignItems="center"
+        flexGrow={1}
+        mt={{ base: 4, md: 0 }}
+      >
+        {/* <Text>Seedlings</Text> */}
+      </Stack>
+
+      <Box
+        display={{ base: isOpen ? "block" : "none", md: "block" }}
+        mt={{ base: 4, md: 0 }}
+      >
+        <Button
+          variant="outline"
+          _hover={{ bg: "teal.700", borderColor: "#AD0F5B" }}
+        >
+          Create account
+        </Button>
+      </Box>
+    </Flex>
+  );
+};
+export default Navbar;
