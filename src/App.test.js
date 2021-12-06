5ec170507dac37670c8fbafee82a61bf043100d7
Zuzanna Tarka <zuzanna.tarka@signavio.com>
2021-12-06 14:29:55 +0100

    test for profile card

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('see if tests are set up', () => {
   render(<App />);
 });
--- src/ProfileCard.test.jsx
+++ src/ProfileCard.test.jsx
@@ -1,7 +1,27 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import React from 'react';
 import ProfileCard from './components/ProfileList/ProfileCard'
 
-test('renders learn react link', () => {
-  render(<ProfileCard name='whatever'/>);
-});
+describe('testing profileCard component', ()=> {
+  it('name is displayed correctly on profile card', () => {
+    const mockName = 'Anja'
+    render(<ProfileCard name={mockName}/>);
+    const card = screen.getByTestId('profileCard')
+    expect(card).toHaveTextContent(mockName)
+  
+  });
+  it('uses correct src',  () => {
+    const mockImage = "https://images.unsplash.com/photo-1606856140824-925bdd3f6681?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+    render(<ProfileCard image={mockImage}/>);
+  
+    const image = screen.getByAltText('profilePic');
+    expect(image.src).toContain(mockImage);
+  });
+  it('test click event on the profile card', () => {
+    const mockCallBack = jest.fn();
+    render(<ProfileCard onClickCard={mockCallBack}/>);
+    const card = screen.getByTestId('profileCard');
+    fireEvent.click(card);
+    expect(mockCallBack).toBeCalled();
+  });
+})
