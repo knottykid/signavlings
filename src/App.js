49e4936a240b00785bb6c9856e87468f8d1a26f2
John Rosario <73444785+knottykid@users.noreply.github.com>
2021-12-07 17:13:46 +0100

    search functionality almost done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 import { ChakraProvider } from "@chakra-ui/react";
@@ -6,25 +6,51 @@ import Navbar from "./components/navbar/Navbar";
 import SeedlingsList from "./pages/Home/SeedlingsList";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [filteredResults, setFilteredResults] = useState([]);
 
-  const [ data, setData] = useState([])
-
-  useEffect ( async ()=>{
+  useEffect(async () => {
     const response = await axios({
-      method: 'get',
-      url: 'https://2b5b7952-d207-4316-bd73-20b489332208.mock.pstmn.io/api/profile',
-    })
-    setData(response.data)
-  }, []
-  )
+      method: "get",
+      url: "https://2b5b7952-d207-4316-bd73-20b489332208.mock.pstmn.io/api/profile",
+    });
+    setData(response.data);
+  }, []);
+
+  const searchItems = async (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchInput !== "") {
+      const filteredData = data.filter((item) => {
+        // console.log("<<", item);
+        return Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchInput.toLowerCase());
+      });
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(data);
+    }
+  };
+
+  const onChange = (e) => searchItems(e.target.value);
 
   return (
     <ChakraProvider>
       <div className="App">
-        <Navbar />
-        <SeedlingsList people={data}/>
+        <Navbar
+          data={data}
+          onChange={onChange}
+          searchInput={searchInput}
+          filteredResults={filteredResults}
+        />
+        <SeedlingsList
+          people={data}
+          searchInput={searchInput}
+          filteredResults={filteredResults}
+        />
       </div>
-
     </ChakraProvider>
   );
 }
--- src/pages/Home/SeedlingsList.jsx
+++ src/pages/Home/SeedlingsList.jsx
@@ -1,34 +1,44 @@
-import React from 'react'
-import ProfileCard from '../../components/ProfileList/ProfileCard'
-import { SimpleGrid, Container} from '@chakra-ui/react'
-import Style from './style.css'
-import { createBreakpoints } from '@chakra-ui/theme-tools'
+import React from "react";
+import ProfileCard from "../../components/ProfileList/ProfileCard";
+import { SimpleGrid, Container } from "@chakra-ui/react";
+import Style from "./style.css";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
 
 const breakpoints = createBreakpoints({
-  sm: '30em',
-  md: '48em',
-  lg: '62em',
-  xl: '80em',
-  '2xl': '96em',
-})
+  sm: "30em",
+  md: "48em",
+  lg: "62em",
+  xl: "80em",
+  "2xl": "96em",
+});
 
+function SeedlingsList({ people, searchInput, filteredResults }) {
+  console.log({ searchInput });
+  console.log({ filteredResults });
+  return (
+    <Container maxW="container.xl">
+      <SimpleGrid columns={4} display={{ md: "flex" }} spacing={10}>
+        {/* //{people.filter(person => person.toLowerCase().includes(searchInput.toLowerCase())).map((filtered, key) => (<div key={key}>{filtered}{' '}</div>))} */}
 
-function SeedlingsList({people}) {
-    return (
-        <Container maxW='container.xl'>
-            <SimpleGrid columns={4} display={{ md: 'flex' }} spacing = {10} >            
-             {people ? (people.map((person)=>{
-        return(
-        <div key ={person.name}>
-          <ProfileCard name={person.name} image={person.photo}/>
-        </div>
-        )
-      })) : null}
-       </SimpleGrid>
-       </Container>
-        
-    )
+        {searchInput.length > 1
+          ? filteredResults.map((filter) => {
+              console.log("ZU", filter.name);
+              return (
+                <div key={filter.name}>
+                  <h1>{filter.name}</h1>
+                </div>
+              );
+            })
+          : people.map((person) => {
+              return (
+                <div key={person.name}>
+                  <ProfileCard name={person.name} image={person.photo} />
+                </div>
+              );
+            })}
+      </SimpleGrid>
+    </Container>
+  );
 }
 
-export default SeedlingsList
-
+export default SeedlingsList;
