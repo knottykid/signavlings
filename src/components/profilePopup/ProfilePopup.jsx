e6f598e8853155d7d0a66b35652db5eb24425c47
Dingong Eboa <cecile.dingong.eboa@sap.com>
2021-12-07 15:57:03 +0100

    card table information

--- src/components/profilePopup/ProfilePopup.jsx
+++ src/components/profilePopup/ProfilePopup.jsx
@@ -9,29 +9,54 @@ import {
     useDisclosure,
     Button,
     Avatar,
+    Table,
+    Tbody,
+    Tr,
+    Td,
   } from '@chakra-ui/react'
 
 const ProfilePopup = ({people, clickedId}) => {
+  const theme = {
+    colors: {
+      purpleSig: '#fafafa'
+    },
+  }
 
     return (people.map((person)=>{
         const { isOpen, onOpen, onClose } = useDisclosure()
         return (
         person.profileId === clickedId ?
             (<React.Fragment>
+              
                 <Button onClick={onOpen}>Open Modal</Button>
                 <Modal isOpen={isOpen} onClose={onClose} >
                 <ModalOverlay />
-                <ModalContent style={{textAlign: 'center', padding: '100px', paddingTop: '20px'}}>
+                <ModalContent style={{textAlign: 'center', paddingTop: '20px'}}>
                 <Avatar size='2xl' name={person.name} src={person.photo} style={{margin: 'auto'}}/>
                     <ModalHeader>{person.name}</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody>
-                    <div>
-                        Location: {person.location}
-                        Join date: {person.joinDate}
-                        Bio: {person.bio}
-                        Role: {person.role}
-                    </div>
+                    <Table variant='striped' colorScheme={theme.colors.purpleSig}>    
+                      <Tbody>
+                        <Tr>
+                          <Td>Location:</Td>
+                          <Td>{person.location}</Td>  
+                        </Tr>
+                        <Tr>
+                          <Td>Join Date: </Td>
+                          <Td>{person.joinDate}</Td>
+                        </Tr>
+                        <Tr>
+                          <Td>Role</Td>
+                          <Td>{person.role}</Td>
+                        </Tr>
+                        <Tr>
+                          <Td>Bio</Td>
+                          <Td>{person.bio}</Td>
+                        </Tr>
+                      </Tbody>                  
+                    </Table>
+                    
                     </ModalBody> 
                 
                 </ModalContent>
