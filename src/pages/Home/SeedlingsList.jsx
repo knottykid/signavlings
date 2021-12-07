23b2274bdbc2f3887ea1f9c0693afd3622c4ccb5
Cintia <cintia@c02f453tmd6r.lan>
2021-12-07 17:05:17 +0100

    add landing page and set routing seedlingsList

--- src/pages/Home/SeedlingsList.jsx
+++ src/pages/Home/SeedlingsList.jsx
@@ -1,17 +1,9 @@
-import React from 'react'
-import ProfileCard from '../../components/ProfileList/ProfileCard'
-import { SimpleGrid, Container} from '@chakra-ui/react'
+import React from 'react';
+import ProfileCard from '../../components/ProfileList/ProfileCard';
+import { SimpleGrid, Container} from '@chakra-ui/react';
 import Style from './style.css'
-import { createBreakpoints } from '@chakra-ui/theme-tools'
-//import { Box } from '@chakra-ui/react'
 
-const breakpoints = createBreakpoints({
-  sm: '30em',
-  md: '48em',
-  lg: '62em',
-  xl: '80em',
-  '2xl': '96em',
-})
+
 
 
 function SeedlingsList({people}) {
@@ -21,7 +13,7 @@ function SeedlingsList({people}) {
             <SimpleGrid minChildWidth='250px' spacingX={5} spacingY={10} >            
              {people ? (people.map((person)=>{
         return(
-        <div key ={person.name}>
+        <div key={person.name}>
           <ProfileCard name={person.name} image={person.photo}/>
         </div>
         )
