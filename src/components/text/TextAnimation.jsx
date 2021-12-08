3a8ecfd7614acde90c37d3f7f337ccaf6039ca43
mintradomundee <mintra.domundee@signavio.com>
2021-12-08 16:18:56 +0100

    text animation on landing page and link button to list

---
+++ src/components/text/TextAnimation.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import AnimatedText from "./AnimatedText";
+import "./TextAnimation.css";
+
+export default function TextAnimation() {
+  const [replay, setReplay] = useState(true);
+  const placeholderText = [
+    { type: "heading1", text: "Welcome to Signavio" },
+    {
+      type: "heading2",
+      text: "Connect with your new team members"
+    }
+  ];
+
+  const container = {
+    visible: {
+      transition: {
+        staggerChildren: 0.025
+      }
+    }
+  };
+
+  const handleReplay = () => {
+    setReplay(!replay);
+    setTimeout(() => {
+      setReplay(true);
+    }, 600);
+  };
+
+  return (
+    <motion.div
+      className="TextAnimation"
+      initial="hidden"
+      animate={replay ? "visible" : "hidden"}
+      variants={container}
+    >
+      <div className="container">
+        {placeholderText.map((item, index) => {
+          return <AnimatedText {...item} key={index} />;
+        })}
+      </div>
+      {/* <button onClick={handleReplay}>
+        Replay <span>⟲</span>
+      </button> */}
+    </motion.div>
+  );
+}
